a69266230760bd9b0abab4925ba7c5f836761b86
man151098 <56228310+man151098@users.noreply.github.com>
2025-02-04 17:58:14 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-052cef05d01020f1e"
-  instance_type = "t2.micro"
+  instance_type = "t2.microoo"
   tags = {
     Owner = "Manjiree"
     TTL   = "100"
